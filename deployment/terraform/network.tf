62b123e57bcfe646c6bf1f72243c31528f231469
Ivan Castanon <ivan.alberto.castanon@gmail.com>
2026-08-20 18:45:11 -0400

    creating nat gateway and private subnet route tables

--- deployment/terraform/network.tf
+++ deployment/terraform/network.tf
@@ -81,3 +81,57 @@ resource "aws_route_table_association" "public" {
   subnet_id      = each.value.id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${local.resource_prefix}-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "fintrack" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[local.availability_zones[0]].id
+
+  depends_on = [aws_internet_gateway.fintrack]
+
+  tags = {
+    Name = "${local.resource_prefix}-nat-${local.availability_zones[0]}"
+  }
+}
+
+resource "aws_route_table" "private_application" {
+  vpc_id = aws_vpc.fintrack.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.fintrack.id
+  }
+
+  tags = {
+    Name = "${local.resource_prefix}-private-application-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private_application" {
+  for_each = aws_subnet.private_application
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private_application.id
+}
+
+resource "aws_route_table" "private_data" {
+  vpc_id = aws_vpc.fintrack.id
+
+  tags = {
+    Name = "${local.resource_prefix}-private-data-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private_data" {
+  for_each = aws_subnet.private_data
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private_data.id
+}
