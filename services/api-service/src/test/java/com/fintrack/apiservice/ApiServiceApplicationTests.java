3ca4c1a009cb0e99503150904d2c4ad80ff3da59
Ivan Castanon <ivan.alberto.castanon@gmail.com>
2026-08-22 20:29:58 -0400

    making the test self contained

--- services/api-service/src/test/java/com/fintrack/apiservice/ApiServiceApplicationTests.java
+++ services/api-service/src/test/java/com/fintrack/apiservice/ApiServiceApplicationTests.java
@@ -2,12 +2,20 @@ package com.fintrack.apiservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
+@Testcontainers
 @SpringBootTest
 class ApiServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    @ServiceConnection
+    static final PostgreSQLContainer POSTGRESQL = new PostgreSQLContainer("postgres:17-alpine");
 
+    @Test
+    void contextLoads() {
+    }
 }
